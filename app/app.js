eb77bd9e8b71b9c94ad4f1280ed72555bb5a2091
peterswallow <peter.swallow@gmail.com>
2016-06-14 13:06:37 +0100

    Hide the tabs sampe, but added the ibeacons code and testing for android to detect Purple iBeacon

--- app/app.js
+++ app/app.js
@@ -7,20 +7,82 @@ import {TabsPage} from './pages/tabs/tabs';
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
+
 export class MyApp {
   static get parameters() {
     return [[Platform]];
   }
 
   constructor(platform) {
-    this.rootPage = TabsPage;
+    //this.rootPage = TabsPage;
+    console.log("hello world 1");
+    //this.logToDom('Hello world AA');
 
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
+      
+      
+
+
+      var delegate = new cordova.plugins.locationManager.Delegate();
+
+
+      delegate.didDetermineStateForRegion = function (pluginResult) {
+
+          console.log('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+
+          cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
+              + JSON.stringify(pluginResult));
+      };
+
+      delegate.didStartMonitoringForRegion = function (pluginResult) {
+          console.log('didStartMonitoringForRegion:', pluginResult);
+
+          //logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
+      };
+
+      delegate.didRangeBeaconsInRegion = function (pluginResult) {
+          console.log('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+      };
+
+      var uuid = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'; //Purple
+      var identifier = 'Purple iBeacon';
+      var minor = 7181;
+      var major = 44956;
+      var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+      cordova.plugins.locationManager.setDelegate(delegate);
+
+      // required in iOS 8+
+      cordova.plugins.locationManager.requestWhenInUseAuthorization(); 
+      // or cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+      cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+          .fail(function(e) { console.error(e); })
+          .done();
+     
+   
+  
+   // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-    });
+   
+   });
+   
   }
-}
+  
+    logToDom(message) {
+          var e = document.createElement('label');
+          e.innerText = message;
+
+          var br = document.createElement('br');
+          var br2 = document.createElement('br');
+          document.body.appendChild(e);
+          document.body.appendChild(br);
+          document.body.appendChild(br2);
+
+          window.scrollTo(0, window.document.height);
+      };
+}  
+
 
 ionicBootstrap(MyApp)
--- platforms/android/assets/www/build/js/app.bundle.js
+++ platforms/android/assets/www/build/js/app.bundle.js
@@ -33,14 +33,68 @@ var MyApp = exports.MyApp = (_dec = (0, _core.Component)({
   function MyApp(platform) {
     _classCallCheck(this, MyApp);
 
-    this.rootPage = _tabs.TabsPage;
+    //this.rootPage = TabsPage;
+    console.log("hello world 1");
+    //this.logToDom('Hello world AA');
 
     platform.ready().then(function () {
+
+      var delegate = new cordova.plugins.locationManager.Delegate();
+
+      delegate.didDetermineStateForRegion = function (pluginResult) {
+
+        console.log('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+
+        cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+      };
+
+      delegate.didStartMonitoringForRegion = function (pluginResult) {
+        console.log('didStartMonitoringForRegion:', pluginResult);
+
+        //logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
+      };
+
+      delegate.didRangeBeaconsInRegion = function (pluginResult) {
+        console.log('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+      };
+
+      var uuid = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'; //Purple
+      var identifier = 'Purple iBeacon';
+      var minor = 7181;
+      var major = 44956;
+      var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+      cordova.plugins.locationManager.setDelegate(delegate);
+
+      // required in iOS 8+
+      cordova.plugins.locationManager.requestWhenInUseAuthorization();
+      // or cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+      cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion).fail(function (e) {
+        console.error(e);
+      }).done();
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       _ionicNative.StatusBar.styleDefault();
     });
   }
+
+  _createClass(MyApp, [{
+    key: 'logToDom',
+    value: function logToDom(message) {
+      var e = document.createElement('label');
+      e.innerText = message;
+
+      var br = document.createElement('br');
+      var br2 = document.createElement('br');
+      document.body.appendChild(e);
+      document.body.appendChild(br);
+      document.body.appendChild(br2);
+
+      window.scrollTo(0, window.document.height);
+    }
+  }]);
 
   return MyApp;
 }()) || _class);
